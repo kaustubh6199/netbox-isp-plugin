d684fb9665b6862340d384d275fce323585f03c8
Kaustubh Jumde <ac130jspooky@gmail.com>
2026-04-25 00:59:25 +0530

    Fix: Add config variable to __init__.py for NetBox plugin compatibility

--- netbox_fiber/__init__.py
+++ netbox_fiber/__init__.py
@@ -1,9 +1,16 @@
 """NetBox Fiber Plugin"""
 
-name = "NetBox Fiber"
-version = "0.1.0"
-author = "Author Name"
-description = "Plugin for managing fiber enclosures, optical splitters, and fiber mappings in PON networks"
-base_url = "fiber"
-required_settings = []
-default_settings = {}
+from netbox.plugins import PluginConfig
+
+
+class NetBoxFiberConfig(PluginConfig):
+    name = "NetBox Fiber"
+    version = "0.1.0"
+    author = "Author Name"
+    description = "Plugin for managing fiber enclosures, optical splitters, and fiber mappings in PON networks"
+    base_url = "fiber"
+    required_settings = []
+    default_settings = {}
+
+
+config = NetBoxFiberConfig
